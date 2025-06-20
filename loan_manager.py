61a8346c70b998bc7ea87a927a265a385379f667
The-Emmanuel-NM <23nm464si@esisalama.org>
2025-06-20 10:20:40 +0100

    Update loan_manager.py

--- loan_manager.py
+++ loan_manager.py
@@ -1,40 +1,25 @@
-import sqlite3
-#connexion a la base des données
-conn = sqlite3.connect("bibliotheque.db")
-cursor = conn.cursor()
-#création des tables
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS livres(
- id INTEGER PRIMARY KEY, titre TEXT, auteur TEXT, disponible BOOLEAN )""")
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS emprunts(
- id INTEGER PRIMARY KEY, utilisateur TEXT, livre_id INTEGER, date_emprunt DATE, date_retour DATE, FOREIGN KEY(livre_id)REFERENCES livres(id))""")
-conn.commit()
 
-#fonction pour emprunter un livre
-def emprunter_livre(utilisateur,livre_id):
-    cursor.execute("SELECT disponible FROM livres WHERE id =?", (livre_id))
-    livre= cursor.fetchone()
-    if livre and livre[0]:
-        cursor.execute("INSERT INTO emprunts(utisateur,livre_id, date_emprunt) VALUES(?,?, DATE('now'))",( utilisateur,livre_id))
-        cursor.execute("UPDATE livres SET disponible = 0 WHERE id =?",(livre_id,))
-        conn.commit()
-        print("livre emprunté avec succès!")
-    else:
-        print("Le livre n'est pas disponible.")
-        
-#fonction pour retourner un livre
-def retourner_livre(livre_id):
-    cursor.execute("UPDATE livres SET disponible = 1 WHERE id = ?",(livre_id,))
-    cursor.execute("UPDATE emprunts SET date_retour = DATE('now') WHERE livre_id =? AND date_retour IS NULL",(livre_id,))
-    conn.commit()
-    print("Livre retourné avec succès!")
+#Module 4 : Gestion des Emprunts
+#Gère l'emprunt et le retour des livres.
 
-#fonction pour filtrer les livres disponibles
-def livres_disponibles():
-    cursor.execute ("SELECT id,titre,auteur FROM livres WHERE disponible = 1")
-    livres = cursor.fetchall()
-    for livre in livres:
-        print(f"ID:{livre[0]},Titre:{livre[1]},Auteur{livre[2]}")
-        
+# Marquer un livre comme emprunté
+def borrow_book(books, book_id, emprunteur):
+    for book in books:
+        if book["id"] == book_id:
+            book["statut"] = f"emprunté par {emprunteur}"
+            break
 
+# Marquer un livre comme retourné
+def return_book(books, book_id):
+    for book in books:
+        if book["id"] == book_id:
+            book["statut"] = "disponible"
+            break
+
+# Lister tous les livres empruntés
+def list_borrowed_books(books):
+    return [book for book in books if book["statut"].startswith("emprunté")]
+
+# Lister les livres disponibles
+def list_available_books(books):
+    return [book for book in books if book["statut"] == "disponible"]
